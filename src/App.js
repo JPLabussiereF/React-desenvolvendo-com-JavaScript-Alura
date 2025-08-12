521fe8fb36aa15a617031b287acfca70d26702f1
JPLabussiereF <labussework@gmail.com>
2025-08-12 11:56:09 -0300

    Aula 1.4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
+import Banner from './componentes/Banner/Banner'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Banner></Banner>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
