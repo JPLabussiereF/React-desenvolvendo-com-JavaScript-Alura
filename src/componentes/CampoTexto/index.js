b35c5dba5441395013453b9bcfec56465ab6cde2
JPLabussiereF <labussework@gmail.com>
2025-08-18 16:44:21 -0300

    Aula - 3.2

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -4,12 +4,19 @@ const CampoTexto = (props) => {
 
     const placeholderModified = `${props.placeholder}...`
 
+    let valor =''
+
+    const aoDigitado = (evento) => {
+        valor=evento.target.value
+        console.log(valor)
+    }
+
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input placeholder={placeholderModified} />
+            <input onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModified} />
         </div>
     )
 }
